f40b14a5b84bde43de4fd4d553697fc9c5ec1c62
knutnergaard <knut.nergaard@gmail.com>
2025-08-27 19:17:42 +0200

    Remove Python as default feature.

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,16 @@
+#[cfg(feature = "python")]
 pub mod bindings;
 pub mod core;
+pub mod test_utils;
 
 pub use core::*;
 
+#[cfg(feature = "python")]
 use pyo3::prelude::*;
+#[cfg(feature = "python")]
 use pyo3::types::PyModule;
 
+#[cfg(feature = "python")]
 #[pymodule]
 fn ufo_gleaner(py: Python<'_>, m: &Bound<'_, PyModule>) -> PyResult<()> {
     // Register classes defined in bindings
